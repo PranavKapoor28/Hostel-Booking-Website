60481ce14d29903a25e87b7536d96a67429030d5
Pranav kapoor <pranavkapoor2k@gmail.com>
2021-11-02 15:37:34 +0530

    Added hostel website

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,19 @@
+<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+				<li><a href="admin-profile.php"><i class="fa fa-user"></i>Admin Profile</a></li>
+					<li><a href="#"><i class="fa fa-desktop"></i> Rooms</a>
+					<ul>
+						<li><a href="create-room.php">Add a Room</a></li>
+						<li><a href="manage-rooms.php">Manage Rooms</a></li>
+					</ul>
+				</li>
+
+				<li><a href="registration.php"><i class="fa fa-user"></i>Student Registration</a></li>
+				<li><a href="manage-students.php"><i class="fa fa-users"></i>Manage Students</a></li>
+				<li><a href="access-log.php"><i class="fa fa-file"></i>User Access logs</a></li>
+
+			
+		</nav>
---
+++ admin/un.php
@@ -0,0 +1,103 @@
+<?php
+session_start();
+include('includes/config.php');
+if(isset($_POST['login']))
+{
+$username=$_POST['username'];
+$password=$_POST['password'];
+$stmt=$mysqli->prepare("SELECT username,email,password,id FROM admin WHERE (userName=?|| email=?) and password=? ");
+				$stmt->bind_param('sss',$username,$username,$password);
+				$stmt->execute();
+				$stmt -> bind_result($username,$username,$password,$id);
+				$rs=$stmt->fetch();
+				$_SESSION['id']=$id;
+				$uip=$_SERVER['REMOTE_ADDR'];
+				$ldate=date('d/m/Y h:i:s', time());
+				if($rs)
+				{
+                //  $insert="INSERT into admin(adminid,ip)VALUES(?,?)";
+   // $stmtins = $mysqli->prepare($insert);
+   // $stmtins->bind_param('sH',$id,$uip);
+    //$res=$stmtins->execute();
+					header("location:admin-profile.php");
+				}
+
+				else
+				{
+					echo "<script>alert('Invalid Username/Email or password');</script>";
+				}
+			}
+				?>
+
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Admin login</title>
+
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+	<link rel="stylesheet" href="css/bootstrap-select.css">
+	<link rel="stylesheet" href="css/fileinput.min.css">
+	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+	<link rel="stylesheet" href="css/style.css">
+</head
+<body>
+	<?php include('../includes/header.php');?>
+	<div class="ts-main-content">
+		<?php include('../includes/sidebar.php');?>
+		<div class="content-wrapper">
+			<div class="container-fluid">
+
+				<div class="row">
+					<div class="col-md-12">
+					
+						<h2 class="page-title">User Login </h2>
+
+						<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+						<div class="well row pt-2x pb-3x bk-light">
+							<div class="col-md-8 col-md-offset-2">
+							
+								<form action="" class="mt" method="post">
+									<label for="" class="text-uppercase text-sm">Email</label>
+									<input type="text" placeholder="Email" name="email" class="form-control mb">
+									<label for="" class="text-uppercase text-sm">Password</label>
+									<input type="password" placeholder="Password" name="password" class="form-control mb">
+									
+
+									<input type="submit" name="login" class="btn btn-primary btn-block" value="login" >
+								</form>
+							</div>
+						</div>
+						<div class="text-center text-light" style="color:black;">
+							<a href="forgot-password.php" style="color:black;">Forgot password?</a>
+						</div>
+					</div>
+				</div>
+						</div>
+							</div>
+						</div>
+					</div>
+				</div> 	
+			</div>
+		</div>
+	</div>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap-select.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+	<script src="js/dataTables.bootstrap.min.js"></script>
+	<script src="js/Chart.min.js"></script>
+	<script src="js/fileinput.js"></script>
+	<script src="js/chartData.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
